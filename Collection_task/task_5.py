6224f5e51bfd29555c154ed8efef90e45cfe1d76
Meghdeepsinh  Chudasama <110728369+meghdeep18@users.noreply.github.com>
2024-11-13 15:33:57 +0530

    Delete Collection_task directory

--- Collection_task/task_5.py
+++
@@ -1,5 +0,0 @@
-l1 =  [1,2,3,4,5,6,7,8,9]
-l2 = [1,2,3,4,5,6,7,8,10]
-
-common_elements = [item for item in l1 if item in l2]
-print(common_elements)
--- Collection_task/task_7.py
+++
@@ -1,6 +0,0 @@
-def remove_duplicates(input_list):  
-    return list(set(input_list))  
- 
-my_list = [1, 2, 2, 3, 4, 4, 5]  
-result = remove_duplicates(my_list)  
-print(result)  
